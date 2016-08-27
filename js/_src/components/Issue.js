efb805306bfbc3586e1c21b1015d3fd6567922ae
胡凡 <hufan.Akarin@Gmail.com>
2016-08-27 22:44:31 +0800

    small change

--- js/_src/components/Issue.js
+++ js/_src/components/Issue.js
@@ -28,14 +28,14 @@ const IssueFooter = ({children})=>(
 );
 
 const Issue = ({issue})=>(
-    <section className="issue">
+    <section className="issue" data-state={issue.state}>
         <IssueHeader title={issue.title} labels={issue.labels} />
         <div  className="issue-body" dangerouslySetInnerHTML={{__html: issue.body_html}}></div>
         <IssueFooter>
             <a href={issue.user.html_url}>{issue.user.login}</a>
             <span> 提交于 {convertDateTime(issue.created_at)} </span>
             {issue.closed_at?<span>处理于 {convertDateTime(issue.closed_at)}</span>:issue.created_at==issue.updated_at?null :<span> 更新于 {convertDateTime(issue.updated_at)} </span>}
-            <a className="button" href={issue.html_url}>查看详细</a>
+            <a className="button" href={issue.html_url} target="_blank">查看详细</a>
         </IssueFooter>
     </section>
 );
